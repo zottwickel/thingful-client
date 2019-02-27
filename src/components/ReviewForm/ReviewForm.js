30947705a75ffa04967a86f7a1fea100ed605002
Joshua Reade Harris <joshua.reade.harris@gmail.com>
2019-02-26 22:15:43 -0800

    Update star rating select list values.

--- src/components/ReviewForm/ReviewForm.js
+++ src/components/ReviewForm/ReviewForm.js
@@ -46,11 +46,11 @@ export default class ReviewForm extends Component {
             name='rating'
             id='rating'
           >
-            <option value='1'>1</option>
-            <option value='2'>2</option>
-            <option value='3'>3</option>
-            <option value='4'>4</option>
-            <option value='5'>5</option>
+            <option value='1'>1 Star</option>
+            <option value='2'>2 Stars</option>
+            <option value='3'>3 Stars</option>
+            <option value='4'>4 Stars</option>
+            <option value='5'>5 Stars</option>
           </select>
         </div>
 
